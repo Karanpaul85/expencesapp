c14bf8743170c26023618ea4932cb0340f6e21be
Karan_Paul <karan.paul@fnp.com>
2024-02-13 17:01:56 +0530

    create pwa app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
+import withPWA from "@ducanh2912/next-pwa";
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig,
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,14 +5,16 @@ const inter = Inter({ subsets: ["latin"] });
 const env = process.env.NODE_ENV;
 
 export const metadata = {
-  metadataBase: new URL(env === "development" ? "http://localhost:3000": process.env["HOST"]),
+  metadataBase: new URL(
+    env === "development" ? "http://localhost:3000" : process.env["HOST"]
+  ),
   openGraph: {
     title: "ExpenseMaster: Your Ultimate Financial Tracking Solution",
     description:
       "Effortlessly manage your finances with our intuitive expense tracking web application. Track expenses, create budgets, and analyze spending patterns all in one place. Stay organized with receipt scanning, customizable categories, and real-time updates. Simplify your financial management and take control of your expenses today!",
     images: [
       {
-        url: "/images/favicon.ico", // Must be an absolute URL
+        url: "/images/budget.png", // Must be an absolute URL
         width: 800,
         height: 600,
         alt: "ExpenseMaster",
@@ -20,22 +22,26 @@ export const metadata = {
     ],
     type: "website",
     locale: "en_US",
-    url: env === "development" ? "http://localhost:3000": process.env["HOST"],
+    url: env === "development" ? "http://localhost:3000" : process.env["HOST"],
     siteName: "ExpenseMaster",
   },
   title: "ExpenseMaster: Your Ultimate Financial Tracking Solution",
   icons: {
-    icon: { url: "/images/favicon.ico", href: "/images/favicon.ico" },
+    icon: { url: "/images/budget.png", href: "/images/budget.png" },
   },
   generator: "ExpenseMaster",
   applicationName: "ExpenseMaster",
   referrer: "origin-when-cross-origin",
   creator: "Karan Singh Paul",
   publisher: "Karan Singh Paul",
+  manifest: "/manifest.json",
   description:
     "Effortlessly manage your finances with our intuitive expense tracking web application. Track expenses, create budgets, and analyze spending patterns all in one place. Stay organized with receipt scanning, customizable categories, and real-time updates. Simplify your financial management and take control of your expenses today!",
   keywords:
     "Expense tracking,Budget management,Financial management,Expense categorization,Receipt scanning,Expense reports,Transaction history,Expense analytics,Expense sharing,Multi-currency support,Expense reminders,Cloud-based expense storage,Mobile expense tracking,Expense approval workflow,Real-time expense tracking,Expense notifications,Tax deductions,Expense synchronization,Customizable expense categories,Secure payment integration",
+};
+export const viewport = {
+  themeColor: "#E75B4C",
 };
 
 export default function RootLayout({ children }) {
